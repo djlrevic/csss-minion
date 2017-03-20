0f14b7d81633a6bec66b33d90271ef0066e7f4f6
Henry Desktop <henrymzhao@gmail.com>
2017-03-19 21:09:15 -0700

    got creating roles working

--- csss-minion.py
+++ csss-minion.py
@@ -6,6 +6,12 @@ description = 'Bot of the CSSS'
 bot = commands.Bot(command_prefix='.', description=description)
 
 token = "***REMOVED***"
+DISCORD_API_ID = '***REMOVED***'
+
+# server = discord.utils.get(self.bot.guilds, id = DISCORD_API_ID)
+# channel = discord.utils.get(server.text_channels, id = DISCORD_API_ID)
+server = discord.Server(id=DISCORD_API_ID)
+roles = server.role_hierarchy
 
 @bot.event
 async def on_ready():
@@ -14,10 +20,24 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-
 @bot.command()
 async def add(left : int, right : int):
     await bot.say(left+right)
 
+@bot.command(pass_context = True)
+async def iam(ctx, entry : str):
+    # print(discord.Role(name="exec"))
+    # await bot.add_roles(ctx.message.author, )
+
+@bot.command(pass_context = True)
+async def newclass(ctx, entry):
+    flag = False
+    for i in range(0, len(ctx.message.author.roles)):
+        if ctx.message.author.roles[i].name == "Regular":
+            flag = True
+    if flag == True:
+       newRole = await bot.create_role(server, name = entry)
+       await bot.add_roles(ctx.message.author, newRole)
+       await bot.say(entry+" class has been created")
 
 bot.run(token)
