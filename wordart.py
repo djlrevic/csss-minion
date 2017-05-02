fd27f1666a34ea8b063872fcb53b10d7d31bd6b3
matthew-morrison <matthewmorrison@hotmail.ca>
2017-05-02 13:31:01 -0700

    cleanup in wordart.py

--- wordart.py
+++ wordart.py
@@ -7,27 +7,21 @@ from psycopg2 import sql
 
 class WordArt:
     tablename = "wordartMessages"
-    createTableQuery = "CREATE TABLE IF NOT EXISTS "+tablename+" (user_id int, msgs varchar(2000), date datetime, UNIQUE(user_id,date))"
     backupArr = ['sad','oh no', 'terrible timing', 'broken', 'sad','sad', 'nuuu',':(', 'broken', 'sad','brokenhearted', 'next time?']
-    backupArr2 = ['wow','wow','nice','wholesome','doggo','neat','nice','kittens','cool','coolstuff']
     
-    insertTableQuery = "INSERT INTO "+tablename+" VALUES ('%s', '%s', '%s')" #INCOMPLETE
-    #ctx.message.author.id, ctx.message.content, ctx.message.timestamp
-    queryTableQuery = "SELECT * FROM "+tablename+" WHERE user_id = '%s'" #works in sqlite <shrugs>
     
     testimg = "testimg.png"
     d = path.dirname(__file__)
     e = "wordart_dir"
     
-       # create table if doesn't exist
+       
     def __init__(self, bot):
         self.bot = bot
         cur = self.bot.conn_wc.cursor()
-       # query = "CREATE TABLE IF NOT EXISTS "+self.tablename+"(user_id int, msgs text, date text)"
-        query = "CREATE TABLE IF NOT EXISTS "+self.tablename+"(user_id bigint, msgs varchar(2000), date text, UNIQUE(user_id, date))"
+        query = "CREATE TABLE IF NOT EXISTS "+self.tablename+"(user_id bigint, msgs varchar(2000), date text, UNIQUE(user_id, date))" # create table if doesn't exist
         cur.execute(query)
         self.bot.conn_wc.commit()
-        cur.close() #??? necessary?
+        cur.close()
        
        
        # open DB and retrieve messages from a userID
@@ -40,18 +34,15 @@ class WordArt:
             arr = []
             for i in range(0, len(entries)):
                 arr.append(entries[i][0])
-            #for j in entries:
-            #    arr.append(j[0])
-            #print(arr)
             return arr           
         except Exception as e:
             print("Something broke. Printing error message: ")
             print(e)
-            return backupArr
+            return self.backupArr
         
     def createImage(self, arr):
         text = " ".join(arr)
-        savedir = path.join(self.d,self.e, "wow.png")
+        savedir = path.join(self.d,self.e, "wow.png") # local image gets overwritten each time. will this break if too many requests?
         wc = WordCloud().generate(text)
         wc.to_file(savedir)
         return savedir
@@ -65,11 +56,6 @@ class WordArt:
         cur.execute(query, data)
         self.bot.conn_wc.commit()
         cur.close()
-        
-        #test function, destroy later
-    @commands.command(pass_context=True)
-    async def upPic(self,ctx):
-        await self.bot.send_file(ctx.message.channel, self.testimg)
 
 
     @commands.command(pass_context=True)
