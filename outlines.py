8e3972b62370472b316899bcc55374512b15b00f
Brendan <bca60@sfu.ca>
2017-05-21 11:14:13 -0700

    fixed some silly errors

--- outlines.py
+++ outlines.py
@@ -14,18 +14,18 @@ class Outlines:
         if len(args) < 2:
             data = {'Error':"Usage: outline <department> <number> (section) (year) (semester)"}
         elif len(args) == 2:
-            data= (courses.print_outline(args[0], args[1]))
+            data = (courses.dict_outline(args[0], args[1]))
         elif len(args) == 3:
-            data = (courses.print_outline(args[0], args[1], args[2]))
+            data = (courses.dict_outline(args[0], args[1], args[2]))
         elif len(args) == 4:
-            data = (courses.print_outline(args[0], args[1], args[2], args[3]))
+            data = (courses.dict_outline(args[0], args[1], args[2], args[3]))
         elif len(args) == 5:
-            data = (courses.print_outline(args[0], args[1], args[2], args[3], args[4]))
+            data = (courses.dict_outline(args[0], args[1], args[2], args[3], args[4]))
         else:
             data = {'Error':"Usage: outline <department> <number> (section) (year) (semester)"}
 
         embed = discord.Embed(title = "SFU Course Outlines", color = discord.Colour(0xa6192e))
-        for key, entry in data.iteritems():
+        for key, entry in data.items():
             embed.add_field(name = key.title(), value = value)
 
 
