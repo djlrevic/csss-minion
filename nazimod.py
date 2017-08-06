39dd5067717bf49ef568c03d5f2005a7c7fdf1cb
matthew-morrison <matthewmorrison@hotmail.ca>
2017-08-06 12:09:05 -0700

    Added nazimod cog. Added channel lock functionality. Added user mute per channel functionality.

---
+++ nazimod.py
@@ -0,0 +1,62 @@
+import discord
+from discord.ext import commands
+
+class Nazimod:
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(pass_context=True)
+    async def lock(self, ctx):
+        ch = ctx.message.channel
+        ch.permissions_for()
+    
+    @commands.command(pass_context=True)
+    async def lock(self, ctx):
+        everyone = ctx.message.server.default_role
+        await ch_perms(ctx.message.channel, everyone, False)
+        await self.bot.say("Locking Channel")        
+      
+    @commands.command(pass_context=True)
+    async def unlock(self, ctx):
+        everyone = ctx.message.server.default_role
+        await ch_perms(ctx.message.channel, everyone, True)
+        await self.bot.say("Unlocking Channel")
+    
+    async def ch_perms(self, channel, user, value):
+        """Helper function"""
+        overwrite = discord.PermissionOverwrite()
+        overwrite.send_messages = value
+        await self.bot.edit_channel_permissions(channel, user, overwrite)
+    
+    
+    @commands.command(pass_context=True)
+    async def unrestrict(self, ctx, *msg):
+        """Undo any restrictions on a user for all channels.
+        Usage: !unrestrict [users..]
+        """
+        channels = ctx.message.server.channels
+        for user in ctx.message.mentions:
+            await self.bot.say("Unrestricting user "+user.name)
+            for ch in channels:
+                await self.ch_perms(ch, user, None) #None sets to default(inherited) value.
+      
+    @commands.command(pass_context=True)
+    async def restrict(self, ctx, *msg):
+        """Restrict user(s) to only post in certain channels.
+        Usage: !restrict [users..] [channels..]
+        Example: !restrict @Henry @Roo #offtopic #bottesting
+        
+        """  
+        channels = ctx.message.server.channels
+        
+        for user in ctx.message.mentions:
+            await self.bot.say("Restricting user "+user.name)
+            for ch in channels:
+                if ch not in ctx.message.channel_mentions:
+                    #print(ch.name,user.nick, "banning user in this channel")
+                    #DO NOT PRINT A LIST OF ALL CHANNELS IN PRODUCTION
+                    await self.ch_perms(ch, user, False)   #ban user from channel.
+        
+        
+def setup(bot):
+    bot.add_cog(Nazimod(bot))
