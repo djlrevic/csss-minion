c1c0d76a19540db5efe2f82d0284c9c023d1e86e
Henry <henrymzhao@gmail.com>
2017-08-25 05:51:39 -0700

    lock and unlock completed

--- modtools.py
+++ modtools.py
@@ -5,27 +5,51 @@ class Modtools:
     def __init__(self, bot):
         self.bot = bot
 
-    # @commands.command(pass_context=True)
-    # async def lock(self, ctx):
-    #     ch = ctx.message.channel
-    #     ch.permissions_for()
-
     @commands.command(pass_context=True)
     async def lock(self, ctx):
         """Locks the current channel."""
+        for role in ctx.message.server.roles:
+            if role.id == '338575090847580160':
+                MUTED_ROLE = role
+            if role.id == '296466915235332106':
+                BOTS_ROLE = role
         if self.minion(ctx):
-            everyone = ctx.message.server.default_role
-            await self.ch_perms(ctx.message.channel, everyone, False)
+            everyone = []
+            for user in ctx.message.server.members:
+                if ctx.message.channel.permissions_for(user).send_messages and BOTS_ROLE not in user.roles: #don't mute bots
+                    everyone.append(user)
+            for user in everyone:
+                await self.bot.add_roles(user, MUTED_ROLE)
             await self.bot.say("Locking Channel")
         else:
             await self.bot.say("You ain't no mod, shoo!")
 
     @commands.command(pass_context=True)
+    async def propagateMute(self, ctx):
+        for role in ctx.message.server.roles:
+            if role.id == '338575090847580160':
+                MUTED_ROLE = role
+
+        overwrite = discord.PermissionOverwrite()
+        for perm, _ in overwrite:
+            setattr(overwrite, perm, False)
+        for channel in ctx.message.server.channels:
+            await self.bot.edit_channel_permissions(channel, MUTED_ROLE, overwrite)
+
+    @commands.command(pass_context=True)
     async def unlock(self, ctx):
         """Unlocks the current channel."""
         if self.minion(ctx):
-            everyone = ctx.message.server.default_role
-            await self.ch_perms(ctx.message.channel, everyone, True)
+            for role in ctx.message.server.roles:
+                if role.id == '338575090847580160':
+                    MUTED_ROLE = role
+            everyone = []
+            for user in ctx.message.server.members:
+                if ctx.message.channel.permissions_for(user).send_messages:
+                    everyone.append(user)
+            for user in everyone:
+                if MUTED_ROLE in user.roles:
+                    await self.bot.remove_roles(user, MUTED_ROLE)
             await self.bot.say("Unlocking Channel")
         else:
             await self.bot.say("You ain't no mod, shoo!")
