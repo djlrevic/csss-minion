598cb4e2418d8c0eba81276e3f5ee81f0a13613c
Henry <henrymzhao@gmail.com>
2017-09-27 14:15:57 -0700

    adding clear function, redone indentation

--- csss-minion.py
+++ csss-minion.py
@@ -30,28 +30,28 @@ database = "experience" #database name used for exp
 
 #check if config file exists, if not, input manually
 if not os.path.isfile(configFile):
-    DISCORD_API_ID = getpass.getpass('Discord API: ')
-    token = getpass.getpass('Token: ')
-    wolframid = getpass.getpass('Wolframalpha: ')
-    ip = "172.93.48.238:25565"
-    description = "Bot of the CSSS"
-    postgrespass = getpass.getpass('Database Password: ')
-    mashape_key = getpass.getpass('Mashape Key: ')
-    local_postgres_pw = getpass.getpass('Database Password: ')
-    imgur_id = getpass.getpass('Imgur client id: ')
+  DISCORD_API_ID = getpass.getpass('Discord API: ')
+  token = getpass.getpass('Token: ')
+  wolframid = getpass.getpass('Wolframalpha: ')
+  ip = "172.93.48.238:25565"
+  description = "Bot of the CSSS"
+  postgrespass = getpass.getpass('Database Password: ')
+  mashape_key = getpass.getpass('Mashape Key: ')
+  local_postgres_pw = getpass.getpass('Database Password: ')
+  imgur_id = getpass.getpass('Imgur client id: ')
 else:
-    #Load the config file
-    config = configparser.ConfigParser()
-    config.read(configFile)
-    description = config.get("Discord", "Description")
-    wolframid = config.get("Wolfram", "TokenId")
-    DISCORD_API_ID = config.get("Discord", "API_ID")
-    token = config.get("Discord", "Token")
-    ip = "172.93.48.238:25565"
-    postgrespass = config.get("Postgres", "Password")
-    mashape_key = config.get("Mashape", "Token")
-    local_postgres_pw = config.get("LocalPG", 'Password')
-    imgur_id = config.get("Imgur", "client_id")
+  #Load the config file
+  config = configparser.ConfigParser()
+  config.read(configFile)
+  description = config.get("Discord", "Description")
+  wolframid = config.get("Wolfram", "TokenId")
+  DISCORD_API_ID = config.get("Discord", "API_ID")
+  token = config.get("Discord", "Token")
+  ip = "172.93.48.238:25565"
+  postgrespass = config.get("Postgres", "Password")
+  mashape_key = config.get("Mashape", "Token")
+  local_postgres_pw = config.get("LocalPG", 'Password')
+  imgur_id = config.get("Imgur", "client_id")
 
 # SQL SETUP------------------------------------------------------------------------------
 urllib.parse.uses_netloc.append("postgres")
@@ -72,7 +72,7 @@ bot.lang_url = config.get("Translate","url")
 EXP_COOLDOWN_TIMER = 60 #seconds
 
 def reloadConfig():
-    pass
+  pass
 
 # creating a 2D empty array for exp queues
 expQueue = []
@@ -80,138 +80,132 @@ bot.conn_wc = psycopg2.connect("port='5432' user='csss_minion' host='localhost' 
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print('------')
-    await bot.change_presence(game=discord.Game(name='Yes my master'))
+  print('Logged in as')
+  print(bot.user.name)
+  print('------')
+  await bot.change_presence(game=discord.Game(name='Yes my master'))
 
 @bot.event
 async def on_message(message):
-    # DATABASE OPERATIONS. DISABLE UNLESS ACTUALLY RUNNING AS SERVICE
-    # print(message.author.name+"#"+message.author.discriminator)
-    if validate(message):
-        await add(message)
-    await bot.process_commands(message)
+  # DATABASE OPERATIONS. DISABLE UNLESS ACTUALLY RUNNING AS SERVICE
+  # print(message.author.name+"#"+message.author.discriminator)
+  if validate(message):
+    await add(message)
+  await bot.process_commands(message)
 
 @bot.command(pass_context = True)
 async def load(ctx, name):
-    if Henry(ctx):
-        try:
-            bot.load_extension(name)
-        except(AttributeError, ImportError) as e:
-            await bot.say("Cog load failed: {}, {}".format(type(e), str(e)))
-            return
-        await bot.say("{} cog loaded.".format(name))
-    else:
-        await bot.say("You ain't my master! Shoo!")
+  if Henry(ctx):
+    try:
+      bot.load_extension(name)
+    except(AttributeError, ImportError) as e:
+      await bot.say("Cog load failed: {}, {}".format(type(e), str(e)))
+      return
+    await bot.say("{} cog loaded.".format(name))
+
 
 @bot.command(pass_context = True)
 async def unload(ctx, name):
-    if Henry(ctx):
-        bot.unload_extension(name)
-        await bot.say("{} cog unloaded".format(name))
-    else:
-        await bot.say("You ain't my master! Shoo!")
+  if Henry(ctx):
+    bot.unload_extension(name)
+    await bot.say("{} cog unloaded".format(name))
 
 @bot.command(pass_context = True)
 async def reload(ctx, name):
-    if Henry(ctx):
-        bot.unload_extension(name)
-        try:
-            bot.load_extension(name)
-        except(AttributeError, ImportError) as e:
-            await bot.say("Cog load failed: {}, {}".format(type(e), str(e)))
-            return
-        await bot.say("`{} cog reloaded`".format(name))
-    else:
-        await bot.say("`You ain't my master! Shoo!``")
+  if Henry(ctx):
+    bot.unload_extension(name)
+    try:
+      bot.load_extension(name)
+    except(AttributeError, ImportError) as e:
+      await bot.say("Cog load failed: {}, {}".format(type(e), str(e)))
+      return
+    await bot.say("`{} cog reloaded`".format(name))
 
 @bot.command(pass_context = True)
 async def exc(ctx, *args):
-    if Henry(ctx):
-        query = " ".join(args)
-        await bot.say("```"+subprocess.getoutput(query)+"```")
-    else:
-        await bot.say("You ain't my master! Shoo!")
+  if Henry(ctx):
+    query = " ".join(args)
+    await bot.say("```"+subprocess.getoutput(query)+"```")
 
 def Henry(ctx):
-    if ctx.message.author.id == "173702138122338305":
-        return True
-    else:
-        return False
+  if ctx.message.author.id == "173702138122338305":
+    return True
+  else:
+    await bot.say("You ain't my master! Shoo!")
+    return False
 
 # pulling all members from the server. Disable unless admin using
 # @bot.command(pass_context = True)
 # async def pull(ctx):
-#     members = ctx.message.server.members
+#   members = ctx.message.server.members
+#   with codex.open('ids.txt', 'a', 'utf-8') as log:
+#       log.write('[')
+#   for i in members:
+#     # target.write('{"name" : "')
+#     # target.write(i.name)
+#     # target.write(', "user_id" : "')
+#     # target.write(i.id)
+#     # target.write('"}, ')
+#     name = bytes(i.name, 'utf-8').decode('utf-8', 'ignore')
 #     with codex.open('ids.txt', 'a', 'utf-8') as log:
-#             log.write('[')
-#     for i in members:
-#         # target.write('{"name" : "')
-#         # target.write(i.name)
-#         # target.write(', "user_id" : "')
-#         # target.write(i.id)
-#         # target.write('"}, ')
-#         name = bytes(i.name, 'utf-8').decode('utf-8', 'ignore')
-#         with codex.open('ids.txt', 'a', 'utf-8') as log:
-#             log.write('{"name" : "' + name + '", "user_id" : "' + i.id + '", "id" : "' + i.discriminator + '"}, ')
-#     with codex.open('ids.txt', 'a', 'utf-8') as log:
-#             log.write(']')
+#       log.write('{"name" : "' + name + '", "user_id" : "' + i.id + '", "id" : "' + i.discriminator + '"}, ')
+#   with codex.open('ids.txt', 'a', 'utf-8') as log:
+#       log.write(']')
 
 async def embed_this_for_me(text, ctx):
-    """Standardized embeddings across cogs"""
-    callingframe = sys._getframe(1)
-    em = discord.Embed(colour=0xfff)
-    em.add_field(name="Results from "+callingframe.f_code.co_name, value=text)
-    #em.set_footer(text="Written by Nos", icon_url="https://cdn.discordapp.com/avatars/173177975045488640/61d53ada7449ce4a3e1fdc13dc0ee21e.png")
-    await bot.send_message(ctx.message.channel, embed=em)
+  """Standardized embeddings across cogs"""
+  callingframe = sys._getframe(1)
+  em = discord.Embed(colour=0xfff)
+  em.add_field(name="Results from "+callingframe.f_code.co_name, value=text)
+  #em.set_footer(text="Written by Nos", icon_url="https://cdn.discordapp.com/avatars/173177975045488640/61d53ada7449ce4a3e1fdc13dc0ee21e.png")
+  await bot.send_message(ctx.message.channel, embed=em)
 
 def fit_msg(msg, maxlen:int=2000):
-    """Split a long message to fit within discord's limits.
-            Uses the following order of division for natural splits:
-            newline > space > any char
-        """
-    msgs = []
+  """Split a long message to fit within discord's limits.
+      Uses the following order of division for natural splits:
+      newline > space > any char
+    """
+  msgs = []
 
-    while len(msg) >= maxlen:
-        if '\n' in msg[:maxlen]:
-            idx = msg[:maxlen].rfind('\n') #find last occurance of newline
-            msgs.append(msg[:idx])
-            msg = msg[idx+1:] #careful which side gets the newline
+  while len(msg) >= maxlen:
+    if '\n' in msg[:maxlen]:
+      idx = msg[:maxlen].rfind('\n') #find last occurance of newline
+      msgs.append(msg[:idx])
+      msg = msg[idx+1:] #careful which side gets the newline
 
-        elif ' ' in msg[:maxlen]:
-            idx = msg[:maxlen].rfind(' ')
-            msgs.append(msg[:idx])
-            msg = msg[idx+1:]
+    elif ' ' in msg[:maxlen]:
+      idx = msg[:maxlen].rfind(' ')
+      msgs.append(msg[:idx])
+      msg = msg[idx+1:]
 
-        else:
-            for x in range(maxlen,0,-1):
-                msgs.append(msg[:x])
-                msg = msg[x:]
-                break;
+    else:
+      for x in range(maxlen,0,-1):
+        msgs.append(msg[:x])
+        msg = msg[x:]
+        break;
 
-    msgs.append(msg)
-    return msgs
+  msgs.append(msg)
+  return msgs
 
 # testing if the bot is alive
 @bot.command()
 async def ping():
-    await bot.say("pong")
+  await bot.say("pong")
 
 if __name__ == "__main__":
-    for extension in startup_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
+  for extension in startup_extensions:
+    try:
+      bot.load_extension(extension)
+    except Exception as e:
+      exc = '{}: {}'.format(type(e).__name__, e)
+      print('Failed to load extension {}\n{}'.format(extension, exc))
 
 @bot.command(pass_context=True)
 async def cogs(ctx):
-    """Lists the currently loaded cogs."""
-    cogs = list(bot.cogs.keys())
-    cogs.sort()
-    await bot.embed_this_for_me("\n".join(cogs), ctx)
+  """Lists the currently loaded cogs."""
+  cogs = list(bot.cogs.keys())
+  cogs.sort()
+  await bot.embed_this_for_me("\n".join(cogs), ctx)
 
 
 # used to update the queue
@@ -219,18 +213,18 @@ async def update():
   await bot.wait_until_ready()
   print("ready")
   while not bot.is_closed:
-      for i, item in enumerate(expQueue):
-          if time.time() - item[1] >= EXP_COOLDOWN_TIMER:
-              print("entry expired")
-              del expQueue[i]
-      await asyncio.sleep(1)
+    for i, item in enumerate(expQueue):
+      if time.time() - item[1] >= EXP_COOLDOWN_TIMER:
+        print("entry expired")
+        del expQueue[i]
+    await asyncio.sleep(1)
 
 # Check if author is currently on cooldown
 def validate(message):
   for item in expQueue:
-    if message.author.id == item[0]:
-      # author on cooldown
-      return False
+  if message.author.id == item[0]:
+    # author on cooldown
+    return False
   # author not on cooldown, add author id and current time to queue
   print("entry added to queue")
   expQueue.append([message.author.id, time.time()])
@@ -241,46 +235,46 @@ async def add(message):
   database = 'experience'
   entry = db_select(database, message.author.id)
   if entry == None:
-    # user not in database
-    exp_amount = random.randint(15, 25)
-    print("entry added to db")
-    db_insert(database, ['name', 'user_id', 'exp', 'level', 'true_experience'], [message.author.name, message.author.id, exp_amount, currentLevel(exp_amount), exp_amount])
+  # user not in database
+  exp_amount = random.randint(15, 25)
+  print("entry added to db")
+  db_insert(database, ['name', 'user_id', 'exp', 'level', 'true_experience'], [message.author.name, message.author.id, exp_amount, currentLevel(exp_amount), exp_amount])
   else:
-    list(entry)
-    changeInExp = random.randint(15, 25)
-    if changeInLevel(changeInExp, entry[3], entry[4]) == 'levelup':
-      # user's levelled up
-      db_update(database, 'level', currentLevel(entry[3]), 'user_id', message.author.id)
-      await bot.send_message(message.channel, "{} has leveled up to {}".format(message.author.name, currentLevel(entry[3])))
+  list(entry)
+  changeInExp = random.randint(15, 25)
+  if changeInLevel(changeInExp, entry[3], entry[4]) == 'levelup':
+    # user's levelled up
+    db_update(database, 'level', currentLevel(entry[3]), 'user_id', message.author.id)
+    await bot.send_message(message.channel, "{} has leveled up to {}".format(message.author.name, currentLevel(entry[3])))
 
-    # if changeInLevel(changeInExp, entry[3], entry[4]) == 'leveldown':
-    #   # user's levelled down
-      # db_update(database, 'level', currentLevel(entry[3]), 'user_id', message.author.id)
-    # update user new experience
-    print("entry update exp")
+  # if changeInLevel(changeInExp, entry[3], entry[4]) == 'leveldown':
+  #   # user's levelled down
+    # db_update(database, 'level', currentLevel(entry[3]), 'user_id', message.author.id)
+  # update user new experience
+  print("entry update exp")
 
-    db_update(database, 'exp', entry[3]+changeInExp, 'user_id', message.author.id)
-    db_update(database, 'true_experience', entry[3]+changeInExp, 'user_id', message.author.id)
+  db_update(database, 'exp', entry[3]+changeInExp, 'user_id', message.author.id)
+  db_update(database, 'true_experience', entry[3]+changeInExp, 'user_id', message.author.id)
 
 
 def changeInLevel(change, experience, currLevel):
   curr_experience = experience + change
   new_level = currentLevel(experience)
   if new_level > currLevel:
-    # user has leveled up
-    return 'levelup'
+  # user has leveled up
+  return 'levelup'
 
 
 # outputs closest level based on total experience
 def currentLevel(experience):
   if experience is 0:
-    return 0
+  return 0
   # grab the template experience list from database
   cur.execute("SELECT level, total_experience FROM template ORDER BY level")
   templateList = cur.fetchall()
   for level in templateList:
-    if experience <= level[1]:
-      return level[0]-1
+  if experience <= level[1]:
+    return level[0]-1
   # should never reach here, error out with -1
   bot.say("Something went wrong.")
   return -1
@@ -298,7 +292,7 @@ def calcLevel(x):
 @bot.command(pass_context = True)
 async def rank(ctx):
   if len(ctx.message.mentions) > 0:
-    ctx.message.author = ctx.message.mentions[0]
+  ctx.message.author = ctx.message.mentions[0]
   cur.execute('SELECT * FROM (SELECT *, row_number() OVER(ORDER BY exp DESC) FROM experience) AS filter WHERE filter.user_id={}'.format(ctx.message.author.id))
   res = list(cur.fetchone())
   cur.execute('SELECT count(*) from experience')
@@ -324,7 +318,7 @@ async def levels(ctx):
   print(res)
   items = []
   for item in res:
-    items.append(['#{}. {}'.format(str(item[6]), str(item[1])), 'Level: {} \nExperience: {}'.format(str(item[4]), str(int(item[3])))])
+  items.append(['#{}. {}'.format(str(item[6]), str(item[1])), 'Level: {} \nExperience: {}'.format(str(item[4]), str(int(item[3])))])
 
   p = Pages(bot, message=ctx.message, entries = items, per_page=10)
   p.embed = discord.Embed(title="Server Level Rankings", colour=discord.Colour(0xdc4643))
@@ -349,6 +343,7 @@ def db_select(database, query, item = '*'):
   return cur.fetchone()
 
 bot.load_extension("wordart") # bot can start and load wordart later.
+bot.Henry = Henry
 bot.embed_this_for_me = embed_this_for_me # attach to bot object so cogs don't need to import main
 bot.fit_msg = fit_msg # attach fit_msg to bot object
 bot.loop.create_task(update())
--- modtools.py
+++ modtools.py
@@ -4,133 +4,146 @@ from discord.ext import commands
 AVOID = ['342891382584770560', '300564141096304650', '360722519860445184', '312038519294001152', '321832332279676928']
 
 class Modtools:
-    def __init__(self, bot):
-        self.bot = bot
-        
-        
-    @commands.command(pass_context=True)
-    async def propagateMute(self, ctx):
-        for role in ctx.message.server.roles:
-            if role.id == '338575090847580160':
-                MUTED_ROLE = role
+  def __init__(self, bot):
+    self.bot = bot
 
-        overwrite = discord.PermissionOverwrite()
-        setattr(overwrite, 'send_messages', False)
-        setattr(overwrite, 'manage_messages', False)
-        setattr(overwrite, 'manage_channels', False)
-        setattr(overwrite, 'manage_server', False)
-        setattr(overwrite, 'manage_nicknames', False)
-        setattr(overwrite, 'manage_roles', False)
-        for channel in ctx.message.server.channels:
-            if channel.id not in AVOID:
-                await self.bot.edit_channel_permissions(channel, MUTED_ROLE, overwrite)
+  @commands.command(pass_context=True)
+  async def propagateMute(self, ctx):
+    if self.bot.Henry(ctx):
+      for role in ctx.message.server.roles:
+        if role.id == '338575090847580160':
+          MUTED_ROLE = role
 
-#    @commands.command(pass_context=True)
-#    async def lock(self, ctx):
-#        """Locks the current channel."""
-#        for role in ctx.message.server.roles:
-#            if role.id == '338575090847580160':
-#                MUTED_ROLE = role
-#            if role.id == '296466915235332106':
-#                BOTS_ROLE = role
-#        if self.minion(ctx):
-#            everyone = []
-#            for user in ctx.message.server.members:
-#                if ctx.message.channel.permissions_for(user).send_messages and BOTS_ROLE not in user.roles: #don't mute bots
-#                    everyone.append(user)
-#            for user in everyone:
-#                await self.bot.add_roles(user, MUTED_ROLE)
-#            await self.bot.say("Locking Channel")
-#        else:
+      overwrite = discord.PermissionOverwrite()
+      setattr(overwrite, 'send_messages', False)
+      setattr(overwrite, 'manage_messages', False)
+      setattr(overwrite, 'manage_channels', False)
+      setattr(overwrite, 'manage_server', False)
+      setattr(overwrite, 'manage_nicknames', False)
+      setattr(overwrite, 'manage_roles', False)
+      for channel in ctx.message.server.channels:
+        if channel.id not in AVOID:
+          await self.bot.edit_channel_permissions(channel, MUTED_ROLE, overwrite)
+
+#  @commands.command(pass_context=True)
+#  async def lock(self, ctx):
+#    """Locks the current channel."""
+#    for role in ctx.message.server.roles:
+#      if role.id == '338575090847580160':
+#        MUTED_ROLE = role
+#      if role.id == '296466915235332106':
+#        BOTS_ROLE = role
+#    if self.minion(ctx):
+#      everyone = []
+#      for user in ctx.message.server.members:
+#        if ctx.message.channel.permissions_for(user).send_messages and BOTS_ROLE not in user.roles: #don't mute bots
+#          everyone.append(user)
+#      for user in everyone:
+#        await self.bot.add_roles(user, MUTED_ROLE)
+#      await self.bot.say("Locking Channel")
+#    else:
+#      await self.bot.say("You ain't no mod, shoo!")
+
+
+
+  @commands.command(pass_context=True)
+  async def unlock(self, ctx):
+    """Unlocks the current channel."""
+    if self.minion(ctx):
+      for role in ctx.message.server.roles:
+        if role.id == '338575090847580160':
+          MUTED_ROLE = role
+      everyone = []
+      for user in ctx.message.server.members:
+        if ctx.message.channel.permissions_for(user).send_messages:
+          everyone.append(user)
+      for user in everyone:
+        if MUTED_ROLE in user.roles:
+          await self.bot.remove_roles(user, MUTED_ROLE)
+      await self.bot.say("Unlocking Channel")
+    else:
+      await self.bot.say("You ain't no mod, shoo!")
+
+  async def ch_perms(self, channel, user, value):
+    """Helper function"""
+    overwrite = discord.PermissionOverwrite()
+    overwrite.send_messages = value
+    await self.bot.edit_channel_permissions(channel, user, overwrite)
+
+  @commands.command(pass_context=True)
+  async def unrestrict(self, ctx, *msg):
+    """Undo any restrictions on a user for all channels.
+    Usage: !unrestrict [users..]
+    """
+    if self.minion(ctx):
+      channels = ctx.message.server.channels
+      for user in ctx.message.mentions:
+        await self.bot.say("Unrestricting user "+user.name)
+        for ch in channels:
+          await self.ch_perms(ch, user, None) #None sets to default(inherited) value.
+    else:
+      await self.bot.say("You ain't no mod, shoo!")
+
+#  @commands.command(pass_context=True)
+#  async def restrict(self, ctx, *msg):
+#    """Restrict user(s) to only post in certain channels.
+#    Usage: !restrict [users..] [channels..]
+#    Example: !restrict @Henry @Roo #offtopic #bottesting
+#
+#    """
+#    if self.minion(ctx):
+#      channels = ctx.message.server.channels
+#      for user in ctx.message.mentions:
+#        await self.bot.say("Restricting user "+user.name)
+#        for ch in channels:
+#          if ch not in ctx.message.channel_mentions:
+#            #print(ch.name,user.nick, "banning user in this channel")
+#            #DO NOT PRINT A LIST OF ALL CHANNELS IN PRODUCTION
+#            await self.ch_perms(ch, user, False)   #ban user from channel.
+#          else:
 #            await self.bot.say("You ain't no mod, shoo!")
 
+  def minion(self, ctx):
+    for role in ctx.message.author.roles:
+      if "314296819272122368" == role.id:
+        return True
+    return False
 
+  @commands.command(pass_context=True)
+  async def modsay(self, ctx, *msg):
+    """Give a stern message.
+       Heavily inspired by brenfan's .em code <3
+    """
+    author = ctx.message.author
+    if author.permissions_in(ctx.message.channel).manage_channels or author.server_permissions.manage_channels:
 
-    @commands.command(pass_context=True)
-    async def unlock(self, ctx):
-        """Unlocks the current channel."""
-        if self.minion(ctx):
-            for role in ctx.message.server.roles:
-                if role.id == '338575090847580160':
-                    MUTED_ROLE = role
-            everyone = []
-            for user in ctx.message.server.members:
-                if ctx.message.channel.permissions_for(user).send_messages:
-                    everyone.append(user)
-            for user in everyone:
-                if MUTED_ROLE in user.roles:
-                    await self.bot.remove_roles(user, MUTED_ROLE)
-            await self.bot.say("Unlocking Channel")
-        else:
-            await self.bot.say("You ain't no mod, shoo!")
+      try:
+        color = red
+      except Exception:
+        color = discord.Colour(0xff0000)
+      string = "\n\n["+" ".join(msg)+"]()"
+      embed = discord.Embed(description = string, color = color, title="An Echo From the Heavens Says...", footer="Moderator Warning")
+      embed.set_author(name=author.display_name, icon_url = author.avatar_url)
+      embed.set_footer(text="Moderator Warning")
+      await self.bot.say(embed=embed)
+    try:
+      await self.bot.delete_message(ctx.message)
+    except Exception:
+      print("Not allowed to delete message")
 
-    async def ch_perms(self, channel, user, value):
-        """Helper function"""
-        overwrite = discord.PermissionOverwrite()
-        overwrite.send_messages = value
-        await self.bot.edit_channel_permissions(channel, user, overwrite)
+  @commands.command(pass_context=True)
+  async def clear(self, ctx, amount):
+    """Clear set amount of messages
+    """
+    if len(ctx.message.mentions) > 0:
+      send_msg = await self.bot.say("Feature coming. Bug Henry.")
+      # await self.bot.purge_from(ctx.message.channel, limit = 50, check=is_me)
+    else:
+      await self.bot.purge_from(ctx.message.channel, limit = amount, check=True)
+      send_msg = await self.bot.say("{} message(s) has been deleted.".format(amount))
 
-    @commands.command(pass_context=True)
-    async def unrestrict(self, ctx, *msg):
-        """Undo any restrictions on a user for all channels.
-        Usage: !unrestrict [users..]
-        """
-        if self.minion(ctx):
-            channels = ctx.message.server.channels
-            for user in ctx.message.mentions:
-                await self.bot.say("Unrestricting user "+user.name)
-                for ch in channels:
-                    await self.ch_perms(ch, user, None) #None sets to default(inherited) value.
-        else:
-            await self.bot.say("You ain't no mod, shoo!")
-
-#    @commands.command(pass_context=True)
-#    async def restrict(self, ctx, *msg):
-#        """Restrict user(s) to only post in certain channels.
-#        Usage: !restrict [users..] [channels..]
-#        Example: !restrict @Henry @Roo #offtopic #bottesting
-#
-#        """
-#        if self.minion(ctx):
-#            channels = ctx.message.server.channels
-#            for user in ctx.message.mentions:
-#                await self.bot.say("Restricting user "+user.name)
-#                for ch in channels:
-#                    if ch not in ctx.message.channel_mentions:
-#                        #print(ch.name,user.nick, "banning user in this channel")
-#                        #DO NOT PRINT A LIST OF ALL CHANNELS IN PRODUCTION
-#                        await self.ch_perms(ch, user, False)   #ban user from channel.
-#                    else:
-#                        await self.bot.say("You ain't no mod, shoo!")
-
-    def minion(self, ctx):
-        for role in ctx.message.author.roles:
-            if "314296819272122368" == role.id:
-                return True
-        return False
-
-    @commands.command(pass_context=True)
-    async def modsay(self, ctx, *msg):
-        """Give a stern message.
-           Heavily inspired by brenfan's .em code <3
-        """
-        author = ctx.message.author
-        if author.permissions_in(ctx.message.channel).manage_channels or author.server_permissions.manage_channels:
-            
-            try:
-                color = red
-            except Exception:
-                color = discord.Colour(0xff0000)
-            string = "\n\n["+" ".join(msg)+"]()"
-            embed = discord.Embed(description = string, color = color, title="An Echo From the Heavens Says...", footer="Moderator Warning")
-            embed.set_author(name=author.display_name, icon_url = author.avatar_url)
-            embed.set_footer(text="Moderator Warning")
-            await self.bot.say(embed=embed)
-        try:
-            await self.bot.delete_message(ctx.message)
-        except Exception:
-            print("Not allowed to delete message")
-
+    await asyncio.sleep(3)
+    await self.bot.delete_message(send_msg)
 
 def setup(bot):
-    bot.add_cog(Modtools(bot))
+  bot.add_cog(Modtools(bot))
